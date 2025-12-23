c129ff60a163b646ca82a968484e9a4b9a4e13b1
niteshvshukla2026-sudo <niteshshukla56@gmail.com>
2025-12-23 17:21:46 +0530

    ul

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 // backend/src/routes/auth.routes.js
 import express from "express";
 import { loginController } from "../controllers/auth.controller.js";
+import { forceResetPassword } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
